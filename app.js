96c076abb467e3cda7e39d4443888af0eaec4285
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-10-31 18:53:38 -0400

    update

--- app.js
+++ app.js
@@ -141,6 +141,6 @@ app.get('/', function(req, res){
 				});
 });
 
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
